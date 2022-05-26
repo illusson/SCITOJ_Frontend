8a3fb40425ec0d9f07bf5284ba023a96935eba3f
张培 <zipeal@yeah.net>
2022-05-26 23:39:12 +0800

    引入路由

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,23 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+G
+src/pages/home/Home.css,2\d\2ddab0b7f37eb86d378abd5a9b979121851e589b
+I
+src/pages/login/Login.css,1\5\15118610b1ee5f27e5507ee7f620095a7aa4ad62
+I
+src/pages/login/Login.tsx,1\4\14ed35de2a1075df5085844a4d5e62f2d62414ef
+A
+src/setupTests.ts,3\9\39129446b425a1d640160c068e4194e96639eedf
+F
+src/reportWebVitals.ts,d\9\d938938695d447dadda115e28781c6541f53fc4f
+F
+src/react-app-env.d.ts,4\a\4a7b99bdbee5792679d347b6474463bf5e14b66d
+<
+src/logo.svg,6\d\6d3a8cd532ece5b4ddbd4b3b6a678ba686d300bc
+=
+src/index.tsx,5\a\5ab44fe33ca96299f51ad4778a73e425b92bf439
+G
+src/pages/home/Home.tsx,1\c\1cbe153c06d6bb59b01eb5c6d1e410568bf9199a
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
+// import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
@@ -17,6 +17,9 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import {useRoutes} from 'react-router-dom'
+
+import routes from '../../routes'
 
 const drawerWidth = 240;
 
@@ -27,10 +30,13 @@ interface LinkTabProps {
 
 function LinkTab(props: LinkTabProps) {
     return (
-        <Tab component="a"
-            onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-                event.preventDefault();
-            }}{...props}/>
+        <div>
+            <Tab component="a"
+                 onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+                     event.preventDefault();
+                 }} {...props}/>
+            {/*<Navigate to={props.href}/>*/}
+        </div>
     );
 }
 
@@ -39,9 +45,14 @@ interface Props {
 }
 
 export default function Home(props: Props) {
+
+    const element = useRoutes(routes)
+
     const {window} = props;
     // 【响应式】移动端侧边栏展开
     const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    // 控制抽屉
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
@@ -50,7 +61,7 @@ export default function Home(props: Props) {
     const gotoPage = (event: React.SyntheticEvent, newValue: number) => {
         setCurrentPage(newValue);
     };
-
+    // 抽屉
     const drawer = (
         <div>
             <Toolbar/>
@@ -78,7 +89,7 @@ export default function Home(props: Props) {
             </List>
         </div>
     );
-
+    // 抽屉容器
     const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
@@ -100,9 +111,11 @@ export default function Home(props: Props) {
                         </Typography>
                         <Tabs value={pageId} onChange={gotoPage} aria-label="nav tabs example"
                               sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
-                            <LinkTab label="Page One" href="/drafts" />
-                            <LinkTab label="Page Two" href="/trash" />
-                            <LinkTab label="Page Three" href="/spam" />
+                            <LinkTab label="题目" href="/subject"/>
+                            <LinkTab label="竞赛" href="/competition" />
+                            <LinkTab label="提交" href="/submit" />
+                            <LinkTab label="作业" href="/work" />
+                            <LinkTab label="关于" href="/about" />
                         </Tabs>
                         <Button color="inherit">登录</Button>
                     </Toolbar>
@@ -123,37 +136,11 @@ export default function Home(props: Props) {
             <main>
                 <Box sx={{flexGrow: 1, p: 3}}>
                     <Toolbar />
-                    <Typography paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                        sapien faucibus et molestie ac.
-                    </Typography>
-                    <Typography paragraph>
-                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                        eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                        neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                        tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                        sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                        tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                        gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                        tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                        eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                        posuere sollicitudin aliquam ultrices sagittis orci a.
-                    </Typography>
+                    {element}
                 </Box>
             </main>
             <footer>
-                <p>Edit <code>src/Home.tsx</code> and save to reload.</p>
+                <p>footer</p>
             </footer>
         </div>
     );
